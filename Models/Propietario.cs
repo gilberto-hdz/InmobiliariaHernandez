8c5aac0fa823c74d90e6ebd786a491b6e295a901
Gilberto Hernandez Amura <ghernandezamura@gmail.com>
2021-03-30 16:37:16 -0300

    Initial commit

---
+++ Models/Propietario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InmobiliariaHernandez.Models
+{
+    public class Propietario
+    {
+        public int Id { get; set; }
+        public string Dni { get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+
+        public override string ToString()
+        {
+            return $"({Id}), {Nombre} {Apellido}";
+        }
+    }
+}
---
+++ Models/PropietarioRepositorio.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InmobiliariaHernandez.Models
+{
+    public class PropietarioRepositorio
+    {
+        private readonly string stringConnection;
+
+        public PropietarioRepositorio()
+        {
+            this.stringConnection = "Server=(localdb)\\MSSQLLocalDB;Database=BDInmobiliaria;Trusted_Connection=True;MultipleActiveResultSets=true";
+        }
+
+        public List<Propietario> ObtenerTodos()
+        {
+            var listaPropietarios = new List<Propietario>();
+
+            return listaPropietarios;
+        }
+    }
+}
